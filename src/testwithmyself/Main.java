00f6557b23b5d40ccdf67ea288b34deab4421ba2
ccuiqqinghhao <ccuiqqinghhao@163.com>
2018-04-20 18:27:57 +0800

    test

---
+++ src/testwithmyself/Main.java
@@ -0,0 +1,22 @@
+package testwithmyself;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String args[]) {
+		// TODO Auto-generated constructor stub
+		
+		Scanner sc=new Scanner(System.in);
+		long h=sc.nextLong();
+		long l=sc.nextLong();
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
